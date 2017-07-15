99df4bff7548115cbd432edb3d798e64f29c13fc
coxm <coxm@users.noreply.github.com>
2017-07-15 13:59:00 +0100

    Merge audio channels into one

--- src/audio/LevelAudio.ts
+++ src/audio/LevelAudio.ts
@@ -4,19 +4,6 @@ import {Engine} from './Engine';
 import {Gun} from './Gun';
 
 
-interface AudioNodeLike {
-	connect(dest: AudioNode, chOut?: number, chIn?: number): AudioNode;
-	disconnect(): void;
-}
-
-
-type AudioNodes = {
-	readonly [key: string]: AudioNodeLike;
-	readonly engine: Engine;
-	readonly gun: Gun;
-}
-
-
 export interface LevelAudioOptions {
 	readonly gunGain: number;
 	readonly engineGain: number;
@@ -31,37 +18,37 @@ export const defaults: LevelAudioOptions = {
 
 export class LevelAudio {
 	private readonly eventsID: symbol = Symbol('LevelAudio');
-	private readonly nodes: Map<symbol, AudioNodes>;
-	// private readonly merger: ChannelMergerNode;
+	private readonly merger: ChannelMergerNode;
+	private readonly master: GainNode;
+	private readonly guns: Gun[];
+	private readonly engines: Engine[];
 
 	constructor(
-		private readonly context: AudioContext,
-		shipIDs: symbol[],
+		context: AudioContext,
+		private readonly shipIDs: symbol[],
 		options?: LevelAudioOptions
 	) {
+		Object.assign(window, {levelAudio: this});
 		options = Object.assign({}, options, defaults);
 
-		const len: number = shipIDs.length;
-		const numConnections: number = 2 * len + 1;
-		const meanGain: number = 1 / numConnections;
-		const engineGain = options!.engineGain * meanGain;
-		const gunGain = options!.gunGain * meanGain;
-
-		this.nodes = new Map();
-		// this.merger = context.createChannelMerger(numConnections);
-
-		for (let i = 0, engIn = 0; i < len; ++i, engIn += 2) {
+		const numActors = shipIDs.length;
+		const numAudioNodes = numActors * 2;
+		this.guns = [];
+		this.engines = [];
+		this.merger = context.createChannelMerger(numAudioNodes);
+		for (let i = 0, input = 0; i < numActors; ++i) {
 			const engine = new Engine(context);
-			engine.gain.value = engineGain;
+			engine.gain.value = 0.2;
 			engine.start();
-			engine.connect(context.destination);
-			// engine.connect(this.merger, 0, engIn);
+			engine.connect(this.merger, 0, input++);
+			this.engines.push(engine);
 
-			const gun = new Gun(context, {maxGain: gunGain});
-			// gun.connect(this.merger, 0, engIn + 1);
-			gun.connect(context.destination);
-			this.nodes.set(shipIDs[i], {engine, gun} as AudioNodes);
+			const gun = new Gun(context, {maxGain: 0.4});
+			gun.connect(this.merger, 0, input++);
+			this.guns.push(gun);
 		}
+		this.master = context.createGain();
+		this.merger.connect(this.master).connect(context.destination);
 	}
 
 	attach(manager: events.Manager): void {
@@ -70,42 +57,34 @@ export class LevelAudio {
 			[events.Category.engineStopped, this.onEngineStopped],
 			[events.Category.gunFired, this.onGunFired],
 		], {id: this.eventsID, context: this});
-		for (let obj of this.nodes.values()) {
-			for (let key in obj) {
-				obj[key].connect(this.context.destination);
-			}
-		}
-		// this.merger.connect(this.merger.context.destination);
+		this.master.connect(this.master.context.destination);
+		this.master.gain.linearRampToValueAtTime(1, 0.5);
 	}
 
 	detach(manager: events.Manager): void {
 		manager.unbatch(this.eventsID);
-		for (let obj of this.nodes.values()) {
-			for (let key in obj) {
-				obj[key].disconnect();
-			}
-		}
-		// this.merger.disconnect();
+		this.master.gain.value = 0;
+		this.master.disconnect();
 	}
 
 	onEngineStarted(ev: events.Event): void {
-		const obj = this.nodes.get(ev.data.driver.actorID);
-		if (obj) {
-			obj.engine.accelerate();
+		const index = this.shipIDs.indexOf(ev.data.driver.actorID);
+		if (index >= 0) {
+			this.engines[index].accelerate();
 		}
 	}
 
 	onEngineStopped(ev: events.Event): void {
-		const obj = this.nodes.get(ev.data.driver.actorID);
-		if (obj) {
-			obj.engine.decelerate();
+		const index = this.shipIDs.indexOf(ev.data.driver.actorID);
+		if (index >= 0) {
+			this.engines[index].decelerate();
 		}
 	}
 
 	onGunFired(ev: events.Event): void {
-		const obj = this.nodes.get(ev.data.actorID);
-		if (obj) {
-			obj.gun.fire()
+		const index = this.shipIDs.indexOf(ev.data.actorID);
+		if (index >= 0) {
+			this.guns[index].fire()
 		}
 	}
 }
